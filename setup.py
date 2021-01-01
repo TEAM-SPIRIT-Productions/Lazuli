f5f621b82ab10b95082a7b886fce7251e235824f
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2021-01-01 17:02:21 +0800

    BUILD: Raise version number; build and pre-release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ classifiers = [
 
 setup(
     name="lazuli",
-    version="0.0.6",
+    version="0.0.7",
     description="A Python-based tool for interacting with AzureMSv316-based databases.",
     long_description=long_description,
     long_description_content_type="text/markdown",
