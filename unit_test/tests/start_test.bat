7bdaa784173ceff34ef4464f9091098651a4520b
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2021-01-07 12:47:08 +0800

    TEST: Add auto-launcher for unit tests

---
+++ unit_test/tests/start_test.bat
@@ -0,0 +1,11 @@
+:: This script allows the user to launch the tests without needing use command line
+@ECHO off
+echo "This script will start Project Lazuli's test sequence."
+echo "Note: The PyTest module is required for this sequence."
+echo "Note: Test files must be put in the repository root, for this sequence."
+echo "Starting up virtual environment..."
+call venv\scripts\activate.bat
+echo "Attempting to test the capabilities of Lazuli..."
+py.test
+echo "Test concluded."
+pause
