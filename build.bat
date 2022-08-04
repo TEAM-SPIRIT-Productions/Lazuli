7ca0de67c3d138d331e149a4cb614616d069e1ba
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-08-04 18:32:18 +0800

    TWEAK: Re-order lines in build script
    deactivate should only be called at the end.

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
-rem echo off
+rem This script generates API docs and pushes them to GitHub, as well as builds
+rem and pushes builds to PyPi
+rem @author KOOKIIE
+echo off
+call venv\scripts\activate.bat
 rem Generate API docs
 echo Now re-generating API Docs...
-call venv\scripts\activate.bat
 call portray on_github_pages -mo lazuli
 echo API Docs pushed to GitHub Pages!
-call venv\scripts\deactivate.bat
 
 rem Build & Distribute
 echo Now building the distribution archives...
@@ -13,5 +15,6 @@ echo Now uploading the distribution archives...
 python -m twine upload --repository testpypi dist/*
 python -m twine upload dist/*
 
+call venv\scripts\deactivate.bat
 echo Sequence completed!
 pause
