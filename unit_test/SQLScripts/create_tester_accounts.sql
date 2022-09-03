60597fc3c0317e67bbe53d159b3a2005cbdd3cf5
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-09-04 00:18:02 +0800

    FIX: (Unit Test) Tester account levels

--- unit_test/SQLScripts/create_tester_accounts.sql
+++ unit_test/SQLScripts/create_tester_accounts.sql
@@ -17,7 +17,7 @@ ON DUPLICATE KEY UPDATE `name`='tester0x01', `password`='test', `2ndpassword`='1
 -- Create admin tester character with Character ID of 900,001
 INSERT INTO `characters` (`id`, `accountid`, `name`, `level`, `str`, `dex`, `luk`, 
 `int`, `hp`, `mp`, `maxhp`, `maxmp`, `hair`, `face`, `map`, `rankpoint`, `gp`, `soul`, `chatban`)
-VALUES (900001, 90001, 'tester0x00', 10, 40, 4, 4, 
+VALUES (900001, 90001, 'tester0x00', 249, 40, 4, 4, 
 4, 500, 500, 500, 500, 36786, 23300, 253000000, 0, 0, 0, 'false')
 ON DUPLICATE KEY UPDATE `accountid`=90001, `name`='tester0x00', `level`=10, `str`=40, `dex`=4, `luk`=4, 
 `int`=4, `hp`=500, `mp`=500, `maxhp`=500, `maxmp`=500, `hair`=36786, `face`=23300, `map`=253000000, `rankpoint`=0, `gp`=0, `soul`=0, `chatban`='false';
@@ -25,7 +25,7 @@ ON DUPLICATE KEY UPDATE `accountid`=90001, `name`='tester0x00', `level`=10, `str
 -- Create admin tester character with Character ID of 900,002
 INSERT INTO `characters` (`id`, `accountid`, `name`, `level`, `str`, `dex`, `luk`, 
 `int`, `hp`, `mp`, `maxhp`, `maxmp`, `hair`, `face`, `map`, `rankpoint`, `gp`, `soul`, `chatban`)
-VALUES (900002, 90002, 'tester0x01', 10, 40, 4, 4, 
+VALUES (900002, 90002, 'tester0x01', 250, 40, 4, 4, 
 4, 500, 500, 500, 500, 36786, 23300, 253000000, 0, 0, 0, 'false')
 ON DUPLICATE KEY UPDATE `accountid`=90002, `name`='tester0x01', `level`=10, `str`=40, `dex`=4, `luk`=4, 
 `int`=4, `hp`=500, `mp`=500, `maxhp`=500, `maxmp`=500, `hair`=36786, `face`=23300, `map`=253000000, `rankpoint`=0, `gp`=0, `soul`=0, `chatban`='false';
