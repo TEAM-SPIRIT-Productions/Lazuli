b9137c5e3c2f77338fe5d3c39f6ddfe49aba448b
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-09-03 21:55:16 +0800

    REFACT: Remove unit test from enduser env

--- unit_test/tests/start_test.bat
+++ unit_test/tests/start_test.bat
@@ -4,7 +4,7 @@ echo "This script will start Project Lazuli's test sequence."
 echo "Note: The PyTest module is required for this sequence."
 echo "Note: Test files must be put in the repository root, for this sequence."
 echo "Starting up virtual environment..."
-call venv\scripts\activate.bat
+call pypi\scripts\activate.bat
 echo "Attempting to test the capabilities of Lazuli..."
 py.test
 echo "Test concluded."
